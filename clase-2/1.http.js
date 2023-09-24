d13869961a0a662b38b333e8a7c1cb137d232f2c
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2023-09-24 12:06:20 +0200

    clase 2 native api

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,15 +1,33 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
-console.log(process.env)
 // $env:PORT="1234"
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hola mundo')
-})
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  if (req.url === '/') {
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  } else if (req.url === '/square-blue.jpg') {
+    fs.readFile('./square-blue.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/jpg')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404
+    res.end('<h1>404</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-  console.log(`Server listening on port ${server.address().port}`)
+  console.log(`Server listening on port http://loclahost:${desiredPort}`)
 })
-
---
+++ clase-2/2.routing.js
@@ -0,0 +1,44 @@
+const http = require('node:http')
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404</h1>')
+      }
+    case 'POST':
+      switch (url) {
+        case '/pokemon/ditto': {
+          let body = ''
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=urf-8' })
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404 Not found</h1>')
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log(`Server listening on port ${1234}`)
+})
