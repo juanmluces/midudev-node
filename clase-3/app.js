40de0b9a6a1f053952e945585b9e750941352c78
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2023-10-10 12:46:36 +0200

    avanzando en clase 3

---
+++ clase-3/app.js
@@ -0,0 +1,51 @@
+const crypto = require('node:crypto')
+const express = require('express')
+const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
+
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+// Todos los recursos que sean MOVIES se identifican con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+  if (result.error) {
+    return res.status(400).json({
+      error: JSON.parse(result.error.message)
+    })
+  }
+  // Esto no sería REST, porque estamos guardando el estado de la aplicación en memoria
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
