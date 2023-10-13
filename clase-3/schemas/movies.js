a647fddd3d65c3770fb395baf05e715de38b16d3
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2023-10-13 11:58:06 +0200

    clase 3

--- clase-3/schemas/movies.js
+++ clase-3/schemas/movies.js
@@ -8,10 +8,10 @@ const movieSchema = z.object({
   year: z.number().int().min(1900).max(2024),
   director: z.string(),
   duration: z.number().int().positive(),
-  rate: z.number().min(0).max(10),
+  rate: z.number().min(0).max(10).default(5),
   poster: z.string().url({ message: 'Poster must be a valid url' }),
   genre: z.array(
-    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'],
+    z.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'],
       {
         required_error: 'Movie genre is required.',
         invalid_type_error: 'Movie genre must be an array of enum Genre'
@@ -23,6 +23,11 @@ function validateMovie (object) {
   return movieSchema.safeParse(object)
 }
 
+function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
+
 module.exports = {
-  validateMovie
+  validateMovie,
+  validatePartialMovie
 }
